75f06cdbaf82f2950bb68f1781e6bf50042a66a8
mayvee <hjwoo0812@naver.com>
2025-05-20 23:26:00 +0900

    주석 추가

--- test_match_pipeline.py
+++ test_match_pipeline.py
@@ -6,13 +6,18 @@ from src.userquery_handler import is_rag_needed
 
 def test_match(user_query:str):
     
+    # RAG 여부 검사
     if(is_rag_needed(user_query)):
+        # match query 생성
         match_query = generate_match_query(user_query)
         print('결과 쿼리: ', match_query)
+        # match query 실행
         records, summary = db.run_query(match_query)
-        print('records: ', records)       
+        print('records: ', records)
+        # 질의, 결과로 응답 생성
         answer = generate_answer(user_query, records)
     else:
+        # RAG 필요없으면 그냥 응답
         answer = generate_answer(user_query)
 
     print('최종 응답:', answer)
