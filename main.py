5f78e8cbf144c735a4bc5eed090e77cd2bc499a9
mayveee <85598812+mayveee@users.noreply.github.com>
2025-05-25 16:57:03 +0900

    /match, /upload (#20)

---
+++ main.py
@@ -0,0 +1,17 @@
+# app/main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes import match, upload
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(upload.router)
+app.include_router(match.router)
---
+++ routes/match.py
@@ -0,0 +1,34 @@
+# routes/match.py
+from fastapi import APIRouter, Request
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from src.chat import chat
+import src.neo4j_runner as db
+from src.dbquery_generator import generate_match_query
+from src.userquery_handler import is_rag_needed
+
+router = APIRouter()
+
+class MessageInput(BaseModel):
+    message: str
+
+@router.post("/match")
+async def match_message(input: MessageInput):
+    user_message = input.message
+    print("📨 사용자 메시지:", user_message)
+    
+    # RAG 여부 검사
+    if(is_rag_needed(user_message)):
+        # match query 생성
+        match_query = generate_match_query(user_message)
+        print('결과 쿼리: ', match_query)
+        # match query 실행
+        records, summary = db.run_query(match_query)
+        print('records: ', records)
+        # 질의, 결과로 응답 생성
+        answer = chat(user_query=user_message, session_id="1234", data=records)
+    else:
+        # RAG 필요없으면 그냥 응답
+        answer = chat(user_message, "1234")
+
+    return JSONResponse(content={"reply": answer})
